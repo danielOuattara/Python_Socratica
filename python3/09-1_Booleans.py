4ad9212e62652fe3cc6c33273e6d0a0f6d19485e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-20 23:55:04 +0200

    09 booleans

---
+++ python3/09-1_Booleans.py
@@ -0,0 +1,38 @@
+# bin/bash !
+# Daniel OUATTARA
+# 02 03 2020
+# Socratica-Python3.x
+
+
+# Boolean values: True, False
+
+a = 3
+b = 5
+
+print(a == b)  # False
+print(a != b)  # True
+print(a < b)  # True
+print(a > b)  # False
+
+print(type(True))  # bool
+print(type(False))  # bool
+print(bool(3.5))  # True
+print(bool(-5))  # True
+print(bool(0))  # False
+
+print(bool('Turing'))  # True
+print(bool(' '))  # space = True
+print(bool(''))  # empty = False
+
+print(str(True))  # 'True'
+print(str(False))  # 'False'
+
+
+print(int(True))  # 1
+print(int(False))  # 0
+
+print(5 + True)  # 6
+print(10 * False)  # 0
+
+
+
