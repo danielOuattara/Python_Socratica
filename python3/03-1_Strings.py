0ff767787396a177d1a657553b241a4f35020538
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-20 23:52:24 +0200

    03 strings

---
+++ python3/03-1_Strings.py
@@ -0,0 +1,26 @@
+# bin/bash !
+# Daniel OUATTARA
+# 24 02 2020
+# Socratica-Python3.x
+#
+
+
+""" Introduction to string"""
+
+message = "Meet me tonight."
+print(message)
+
+message2 = 'The clock strikes at midnight.'
+print(message2)
+
+message3 = 'I\'m looking for someone to share in an adventure'
+print(message3)
+
+message4 = "I'm still looking for someone to share in an adventure"
+print(message4)
+
+message5 = '''The phrase "Beam me up", Scott  was never said on Star Trek "'''
+print(message5)
+
+message6 = """The phrase still "Beam me up", Scott  was never said on Star Trek """
+print(message6)
