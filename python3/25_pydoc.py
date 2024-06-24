dcee5b5132165643b46283edd850b68f840eba26
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-24 23:37:13 +0200

    25 PyDoc

---
+++ python3/25_pydoc.py
@@ -0,0 +1,34 @@
+# !bin/bash
+# Daniel OUATTARA
+# 24 06 2024
+
+
+"""
+MetaDocumentation
+------------------
+python3 -m pydoc
+python3 -m pydoc math
+python3 -m pydoc tuple
+python3 -m pydoc pow
+
+python3
+>>> help(pow)
+
+python3 -m pydoc -k <keyword>
+python3 -m pydoc -k ftp
+python3 -m pydoc -k async
+python3 -m pydoc -k sql
+
+
+python3 -m pydoc -p <port>
+python3 -m pydoc -p 3000
+
+
+python3 -m pydoc -b <port>
+python3 -m pydoc -b 3000
+
+
+python3 -m pydoc -m <name>
+python3 -m pydoc -b 3000
+
+"""
