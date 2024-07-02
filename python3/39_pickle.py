693998c2ecb24512eb6e59f96d2c3ba01c26b876
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-02 23:32:52 +0200

    39 Pickle module

---
+++ python3/39_pickle.py
@@ -0,0 +1,20 @@
+# Daniel OUATTARA
+# 02 07 2024
+
+
+"""https://docs.python.org/3.11/library/pickle.html
+
+
+The pickle module implements binary protocols for serializing 
+and de-serializing a Python object structure. 
+
+“Pickling” is the process whereby a Python object hierarchy is 
+converted into a byte stream, and “unpickling” is the inverse 
+operation, whereby a byte stream (from a binary file or bytes-like 
+object) is converted back into an object hierarchy. 
+
+Pickling (and unpickling) is alternatively known as “serialization”, 
+“marshalling,” [1] or “flattening”; however, to avoid confusion, 
+the terms used here  are “pickling” and “unpickling”.
+
+"""
