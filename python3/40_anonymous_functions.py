3081ffe0dec0eccd4f2b0200c2bffec2f3ff66ea
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-02 23:33:25 +0200

    40 anonymous functions

---
+++ python3/40_anonymous_functions.py
@@ -0,0 +1,26 @@
+
+# Daniel OUATTARA
+# 02 07 2024
+
+
+"""
+https://docs.python.org/3.11/reference/expressions.html#lambda
+
+lambda_expr ::=  "lambda" [parameter_list] ":" expression
+
+Lambda expressions (sometimes called lambda forms) are used 
+to create anonymous functions. 
+
+The expression lambda parameters: expression yields a function 
+object. The unnamed object behaves like a function object defined 
+with:
+
+def <lambda>(parameters):
+    return expression
+
+See section Function definitions for the syntax of parameter lists. 
+
+Note that functions created with lambda expressions cannot contain 
+statements or annotations.
+
+"""
