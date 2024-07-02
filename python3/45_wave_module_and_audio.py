a4641440103899baafa44f4bdae9a6e99f3b0f29
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-02 23:34:44 +0200

    45 wave module & audio

---
+++ python3/45_wave_module_and_audio.py
@@ -0,0 +1,13 @@
+# Daniel OUATTARA
+# 02 07 2024
+
+
+"""
+https://docs.python.org/3.11/library/wave.html#module-wave
+
+audio file: stored as uncompressed wav files
+
+- wave module: handle reading and writing audio to *.wav files
+
+- audio file params = frequency (~48kHz), bitrate(~32-bit), channels(2)
+"""
